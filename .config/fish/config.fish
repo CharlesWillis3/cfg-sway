6781b342777bce3636ce24dc2dd5b7114248f295
Charles Willis <5862883+CharlesWillis3@users.noreply.github.com>
2020-12-06 19:22:10 +0000

    sway colors, use bemenu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ set -xg XDG_CURRENT_DESKTOP Unity
 set -xg EDITOR /sbin/nvim
 set -xg MANPAGER "nvim +Man!"
 set -xg LC_COLLATE C
+set -xg BEMENU_BACKEND wayland
 
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
