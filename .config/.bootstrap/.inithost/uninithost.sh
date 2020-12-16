90e04cf327f57d91394eae83da4b0132c454cc62
Charles Willis <5862883+CharlesWillis3@users.noreply.github.com>
2020-12-16 01:18:20 +0000

    new waybar style

--- .config/.bootstrap/.inithost/uninithost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ "$1" != "--confirm" ]] && echo "Destructive! Must pass --confirm" && exit 
-
-sudo pacman --noconfirm -R chromium
-sudo pacman --noconfirm -R github-cli
-export BROWSER=
-sudo rm -rf /etc/chromium
-shred -u  $HOME/.ssh/id_inithost* 
--- .config/.bootstrap/post-install.fish
+++ .config/.bootstrap/post-install.fish
@@ -1,6 +1,15 @@
 #!/bin/fish
 
 set DIR (dirname (status --current-filename))
+
+if read_confirm "enable bootsplash-systemd services"
+  systemctl enable bootsplash-ask-password-console.path
+  systemctl enable bootsplash-ask-password-console.service
+  systemctl enable bootsplash-hide-when-booted.service
+  systemctl enable bootsplash-show-on-shutdown.service
+  systemctl mask systemd-ask-password-console.path
+end
+
 if read_confirm "enable ly dm service"
   sudo systemctl enable ly.service
   sudo systemctl disable getty@tty1.service
