6cce6375bdaa47e656074bf824159580eb25abab
Charles Willis <5862883+CharlesWillis3@users.noreply.github.com>
2020-12-16 18:33:21 +0000

    new waybar style, updates to sway binds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,8 @@ set -xg MOZ_ENABLE_WAYLAND 1
 set -xg TERMINAL alacritty
 set -xg NNN_PLUG 'f:finder;o:fzopen;d:diffs;t:nmount;v:imgview;l:launch;p:preview-tui;b:-_|chromium $nnn;n:-_|/usr/bin/chromiumw $nnn;x:_chmod +x $nnn*'
 
+ssh-add  ~/.ssh/id_inithost
+
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
       set-next-bg -s northern-lights
